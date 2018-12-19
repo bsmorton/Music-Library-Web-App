c52930c33388c718da3a2ef923e2dceceda534c4
bsmorton <bsmorton@uci.edu>
2018-12-19 12:47:12 -0800

    Add files via upload
    allows user to add songs

--- Music Library/assets/js/musicLibrary.js
+++ Music Library/assets/js/musicLibrary.js
@@ -1,12 +1,25 @@
 // click to play/stop song
-$("li").on("click", function(){
+$("ul").on("click", "li", function(){
     $(this).toggleClass("playing");
 })
 
 // click to delete song
-$("span").on("click", function(event){
+$("ul").on("click", "span",function(event){
     $(this).parent().fadeOut(1000, function(){
         $(this).remove();
     });
     event.stopPropagation();
 })
+
+// add a song to the library
+$("input[type='text']").keypress(function(event){
+    if(event.which === 13){
+        // grabs new song name from input
+        var newSong = $(this).val();
+        // creates new song
+        $("ul").append("<li><span>X</span> "+newSong+"</li>");
+        // clear text input
+        $(this).val("");
+    }
+})
+
